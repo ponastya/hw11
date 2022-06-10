49ade80783379cca90b113a9d60efef831377ec2
ponastya <nastya8po@gmail.com>
2022-06-10 14:05:29 +0300

    шаг 3 и 4 полностью - /candidate/<int:uid> и переход на страницу кандидата через главную

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from utils import *
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 @app.route("/")
 def main_page():
     candidates = load_candidates()
-    return render_template("list.html", candidate=candidates)
+    workers = {}
+    for candidate in candidates:
+        workers[candidate['name']] = candidate['id']
+    print(workers)
+    return render_template("list.html",
+                           candidates_name=workers,
+                           )
+
+@app.route("/candidate/<int:uid>")
+def candidates_by_id(uid: int):
+    candidate = get_candidate(uid)
+    return render_template("card.html",
+                           name=candidate['name'],
+                           position=candidate['position'],
+                           skills=candidate['skills'],
+                           img=candidate['picture']
+                           )
+
 
 app.run()
